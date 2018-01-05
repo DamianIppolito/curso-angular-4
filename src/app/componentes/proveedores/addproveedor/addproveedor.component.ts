192f65dc15e2d80aa4d0968ba5f68ddae14ad8f3
Damian Ippolito <dippolito@dimaticasoftware.com>
2018-01-05 15:10:41 +0100

    Seccion 7 - 38 - Template Driven II

--- src/app/componentes/proveedores/addproveedor/addproveedor.component.ts
+++ src/app/componentes/proveedores/addproveedor/addproveedor.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-addproveedor',
   templateUrl: './addproveedor.component.html',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddproveedorComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formProv') formProv : NgForm
+
+  proveedor : any;
+
+  constructor() {
+    this.proveedor = {
+      nombre : '',
+      cif : '',
+      direccion : '',
+      cp : '',
+      localidad : '',
+      provincia : '',
+      telefono : null,
+      email : '',
+      contacto : ''
+    }
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.proveedor.nombre = this.formProv.value.nombre;
+    this.proveedor.cif = this.formProv.value.cif;
+    this.proveedor.direccion = this.formProv.value.direccion;
+    this.proveedor.cp = this.formProv.value.cp;
+    this.proveedor.localidad = this.formProv.value.localidad;
+    this.proveedor.provincia = this.formProv.value.provincia;
+    this.proveedor.telefono = this.formProv.value.telefono;
+    this.proveedor.email = this.formProv.value.email;
+    this.proveedor.contacto = this.formProv.value.contacto;
+
+    this.formProv.reset();
   }
 
 }
